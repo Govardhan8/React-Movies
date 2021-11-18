c103b76af89be9fbe19f55f6fb8840935bb1c953
Govardhan8 <govardhanmurthy007@gmail.com>
2021-11-18 13:24:53 +0530

    adding useReducer in colorgame input box

--- src/materialComponents/Games/ColorGame.js
+++ src/materialComponents/Games/ColorGame.js
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import Button from '@mui/material/Button'
 
 const ColorGame = () => {
 	const [color, setColor] = useState('white')
 	const [colors, setColors] = useState([])
+
+	//Using useReducer hook
+	const initialState = { bgcolor: '' }
+	const reducer = (state, action) => {
+		switch (action.type) {
+			case 'setcolor':
+				return { bgcolor: action.payload }
+			default:
+				return state
+		}
+	}
+	const [state, dispatch] = useReducer(reducer, initialState)
+
 	return (
 		<>
 			<p className='gameHeading'>ColorGame</p>
@@ -26,6 +39,15 @@ const ColorGame = () => {
 				>
 					Add Color
 				</Button>
+				<input
+					className='inputField'
+					placeholder='color'
+					value={state.bgcolor}
+					onChange={(e) => {
+						dispatch({ type: 'setcolor', payload: e.target.value })
+					}}
+					style={{ backgroundColor: state.bgcolor }}
+				/>
 			</div>
 			<div className='colors'>
 				{colors.map((c, i) => (
